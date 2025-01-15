438047fd17373d55a0ff19e2cfcab3636b7a1b39
vedant185raut <vedant185raut@gmail.com>
2025-01-15 11:01:07 +0530

    Initial commit for static website

--- Hello-World-Static-Web-App/js/hello.js
+++ Hello-World-Static-Web-App/js/hello.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello Krishna!";
+  greeting.innerHTML = "Hello Vedant";
 
   element.appendChild(greeting);
 
